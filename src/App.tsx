84ebdf1fd26518d28472b0333ba3f29020b0f9fb
Emmanuel Antony <emmanuelantony2000@gmail.com>
2021-04-30 16:12:42 +0530

    Fixed issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useMemo } from 'react';
 import styled from 'styled-components';
+
+import { ethers } from 'ethers';
+import Web3 from 'web3';
 import { Button, Loader, Title } from '@gnosis.pm/safe-react-components';
-import { useSafeAppsSDK } from '@gnosis.pm/safe-apps-react-sdk';
+import { useSafeAppsSDK, SafeProvider } from '@gnosis.pm/safe-apps-react-sdk';
+
+import { SafeAppProvider } from '@gnosis.pm/safe-apps-provider';
+import { ConnextModal } from '@connext/vector-modal';
+
+declare global {
+  interface Window {
+    ethereum: any;
+    web3: any;
+  }
+}
+
 
 const Container = styled.form`
   margin-bottom: 2rem;
@@ -16,52 +30,46 @@ const Container = styled.form`
 
 const App: React.FC = () => {
   const { sdk, safe } = useSafeAppsSDK();
-  const [submitting, setSubmitting] = useState(false);
+  const web3Provider = new SafeAppProvider(safe, sdk);
 
-  const submitTx = useCallback(async () => {
-    setSubmitting(true);
-    try {
-      const { safeTxHash } = await sdk.txs.send({
-        txs: [
-          {
-            to: safe.safeAddress,
-            value: '0',
-            data: '0x',
-          },
-        ],
-      });
-      console.log({ safeTxHash });
-      const safeTx = await sdk.txs.getBySafeTxHash(safeTxHash);
-      console.log({ safeTx });
-    } catch (e) {
-      console.error(e);
+  const ethEnabled = async () => {
+    if (window.ethereum) {
+      await window.ethereum.send('eth_requestAccounts');
+      window.web3 = new Web3(window.ethereum);
+      return true;
     }
-    setSubmitting(false);
-  }, [safe, sdk]);
+    return false;
+  }
+  if (!ethEnabled()) {
+    alert("Please install MetaMask to use this dApp!");
+  }
+  const [showModal, setShowModal] = React.useState(false);
+
 
   return (
-    <Container>
-      <Title size="md">{safe.safeAddress}</Title>
-      {submitting ? (
-        <>
-          <Loader size="md" />
-          <br />
-          <Button
-            size="lg"
-            color="secondary"
-            onClick={() => {
-              setSubmitting(false);
-            }}
-          >
-            Cancel
-          </Button>
-        </>
-      ) : (
-        <Button size="lg" color="primary" onClick={submitTx}>
-          Submit
-        </Button>
-      )}
-    </Container>
+    <>
+      <Container>
+        <Button size="lg" color="primary" onClick={() => setShowModal(true)}>Hello World</Button>
+        <ConnextModal
+          showModal={showModal}
+          onClose={() => setShowModal(false)}
+          onReady={params => console.log('MODAL IS READY =======>', params)}
+          injectedProvider={web3Provider}
+          loginProvider={window.ethereum}
+          withdrawalAddress={'0x7BF0C0259DA2db1Cc9A484945722221c5B800139'}
+          routerPublicIdentifier="vector7tbbTxQp8ppEQUgPsbGiTrVdapLdU5dH7zTbVuXRf1M4CEBU9Q"
+          depositAssetId={'0x0000000000000000000000000000000000000000'}
+          depositChainProvider="https://rinkeby.infura.io/v3/31a0f6f85580403986edab0be5f7673c"
+          depositChainId={4}
+          withdrawAssetId={'0x0000000000000000000000000000000000000000'} // likely use injected signer
+          withdrawChainProvider="https://kovan.infura.io/v3/31a0f6f85580403986edab0be5f7673c"
+          withdrawChainId={42}
+          onDepositTxCreated={(txHash) => { console.log('Deposit Tx Created =======>', txHash) }}
+        />
+        { }
+      </Container>
+    </>
+
   );
 };
 
